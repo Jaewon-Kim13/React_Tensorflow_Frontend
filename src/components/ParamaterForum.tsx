8caf6b4669cb747ed9b2c7d73eb7a6668b756ff2
Jaewon Kim <jaewon.kim1326@gmail.com>
2024-06-30 22:02:16 -0500

    added stuff

--- src/components/ParamaterForum.tsx
+++ src/components/ParamaterForum.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function ParamaterForum() {
+interface Props {
+	hiddenLayers: any;
+	setHiddenLayers: any;
+	activation: any;
+	setActivation: any;
+	lambda: any;
+	setLambda: any;
+	loss: any;
+	setLoss: any;
+}
+
+function ParamaterForum({
+	hiddenLayers,
+	setHiddenLayers,
+	activation,
+	setActivation,
+	lambda,
+	setLambda,
+	loss,
+	setLoss,
+}: Props) {
+	useEffect(() => {}, [hiddenLayers, activation, lambda, loss]);
+
 	return <div>ParamaterForum</div>;
 }
 
 export default ParamaterForum;
+
+//will use switch function to return the correct loss function!
+function lossToFunction(loss: string) {
+	let lossFunction;
+
+	return lossFunction;
+}
+
+//will use switch function to return the correct activation function!
+function activationToFunctiopn(activation: string) {
+	let activationFunction;
+
+	return activationFunction;
+}
--- src/pages/NeuralBuilder.tsx
+++ src/pages/NeuralBuilder.tsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
+import { Layer, InputLayer } from "../scripts/Interfaces";
+import NerualNetwork from "../scripts/NeuralNetwork";
+
+import GraphVisualizer from "../components/GraphVisualizer";
+import InputData from "../components/InputData";
+import NeuralNetwork from "../components/NeuralNetwork";
+import ParamaterForum from "../components/ParamaterForum";
 
 export default function NeuralBuilder() {
+	//state for the whole network: NOTE THIS OBJECT IS IMMUTABLE!!!!
+	const [network, setNetwork] = useState<NerualNetwork>();
+	//New section for loading/saving model: THIS NEEDS TO BE CONNECTED TO THE DB
+	const [userModels, setUserModels] = useState<any>();
+	const [defaultModels, setDefaultModels] = useState<any>();
+	//States for network parameters
+	const [hiddenLayers, setHiddenLayers] = useState<Layer[]>();
+	const [activation, setActivation] = useState();
+	const [lambda, setLambda] = useState();
+	const [loss, setLoss] = useState();
+	//These States depend on input data and desired output
+	const [inputLayer, setInputLayer] = useState<InputLayer>();
+	const [inputShape, setInputShape] = useState<number>();
+
+	const [numClasses, setNumClasses] = useState<number>();
+	const [outputLayer, setOutputLayer] = useState<Layer>();
+	//visualisation will use all of these!!!
+
 	return (
 		<>
 			<div className="neural-container">
-				<div className="input-network-container">
-					<div className="input-container"></div>
-					<div className="network-container"></div>
-				</div>
+				<div className="input-network-container"></div>
 				<div className="input-visual-container">
 					<div className="input-container"></div>
 					<div className="visual-container"></div>
